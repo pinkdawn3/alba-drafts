e76eecbcf7cb5b7328e134e05a3ed2bc07b6acaf
Alba <albadetaoro@gmail.com>
2026-01-23 19:00:29 +0000

    test: added test for about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,18 @@
-import { BrowserRouter, NavLink, Route, Routes } from "react-router";
-import About from "../About/About";
-import Home from "../Home/Home";
+import { NavLink } from "react-router";
 
 function Header() {
   return (
-    <BrowserRouter>
-      <nav className="header">
-        <NavLink to="/" className={"links-header"}>
-          Home
-        </NavLink>
-        <NavLink to="/projects" className={"links-header"}>
-          Projects
-        </NavLink>
-        <NavLink to="/about" className={"links-header"}>
-          About me
-        </NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <nav className="header">
+      <NavLink to="/" className={"links-header"}>
+        Home
+      </NavLink>
+      <NavLink to="/projects" className={"links-header"}>
+        Projects
+      </NavLink>
+      <NavLink to="/about" className={"links-header"}>
+        About me
+      </NavLink>
+    </nav>
   );
 }
 
