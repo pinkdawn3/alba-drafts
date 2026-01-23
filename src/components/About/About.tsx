dee59a5400b8b18434a47bf6af77ea38ce611c10
Alba <albadetaoro@gmail.com>
2026-01-23 18:26:19 +0000

    tab: changed name and icon of tab title

---
+++ src/components/About/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <>
+      <p>Esto es una prueba. Me llamo Alba y me gusta el rosa.</p>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <p>Esta página web es para mostrar las cosas que sé hacer.</p>
+    </>
+  );
+}
+
+export default Home;
