d4ec3988d8c1b95e5f7a4771f45d5efc5cde589b
Alba <albadetaoro@gmail.com>
2026-01-23 19:00:29 +0000

    test: added test for about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,7 @@
 function About() {
   return (
     <>
+      <h1>Sobre mí</h1>
       <p>Esto es una prueba. Me llamo Alba y me gusta el rosa.</p>
     </>
   );
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,5 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../../App";
+import { BrowserRouter } from "react-router";
+
 describe("Header", () => {
+  const user = userEvent.setup();
+
   //Navigates between components
 
-  it("should navigate to About component", () => {});
+  it("should navigate to About component", async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const aboutButton = screen.getByText(/about/i);
+
+    expect(aboutButton).toBeInTheDocument();
+
+    await user.click(screen.getByRole("link", { name: /about/i }));
+
+    expect(screen.getByRole("heading", { name: /sobre/i })).toBeInTheDocument();
+  });
 });
