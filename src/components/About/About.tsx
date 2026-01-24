856882d7a8596bd901424f607871739e5aa70ece
Alba <albadetaoro@gmail.com>
2026-01-24 22:24:47 +0000

    docs: added readme

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,11 @@
 function About() {
   return (
-    <main>
-      <h2>¿Quién soy?</h2>
-      <p>Me llamo Alba y me gusta el rosa.</p>
+    <main className="flex flex-col py-10 justify-center space-y-2 text-center">
+      <p>
+        My name is Alba, and I'm a developer from Canary Islands, Spain. This
+        website is still in construction, and I will be adding more stuff along
+        the way.
+      </p>
     </main>
   );
 }
