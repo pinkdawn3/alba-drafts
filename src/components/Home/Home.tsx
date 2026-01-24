67c148c646f26e38bfa692d7a3038162afbdf6d8
Alba <albadetaoro@gmail.com>
2026-01-24 18:28:33 +0000

    feat: cart widget number updates in real time

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,11 @@
 function Home() {
   return (
     <main className="flex flex-col h-screen items-center justify-center space-y-2 text-center">
-      <h1>¡Buenas!</h1>
-      <p>Esta página web es para mostrar las cosas que sé hacer.</p>
+      <h1>Hello!</h1>
+      <p className="text-xl">
+        My name is Alba, and I'm a front-end developer! <br /> This website is a
+        porfolio with the things I know how to do.
+      </p>
     </main>
   );
 }
--- src/components/Projects/Shop/Cart/Cart.tsx
+++ src/components/Projects/Shop/Cart/Cart.tsx
@@ -1,16 +1,18 @@
-import { useEffect, type FunctionComponent } from "react";
-import { useLocation } from "react-router";
+import { type FunctionComponent } from "react";
+
 import useLocalStorageState from "use-local-storage-state";
 import type { CartProps } from "../Products/Products";
-import { Quantifier, type Operation } from "../Quantifyer/Quantifyer";
+import { type Operation, Quantifier } from "../Quantifier/Quantifier";
+import { useNavigate } from "react-router";
 
 export const Cart: FunctionComponent = () => {
   const [cart, setCart] = useLocalStorageState<CartProps>("cart", {});
-  const location = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
+  const navigate = useNavigate();
+
+  const navigateToShop = () => {
+    navigate("/projects/shop/");
+  };
 
   const handleRemoveProduct = (productId: number): void => {
     setCart((prevCart) => {
@@ -48,56 +50,78 @@ export const Cart: FunctionComponent = () => {
   );
 
   return (
-    <section>
-      <h1>Cart</h1>
+    <div className="mx-auto max-w-2xl px-4 py-5 sm:px-6 sm:py- lg:max-w-7xl lg:px-8">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-200">Cart</h2>
+      <div className="flow-root py-5">
+        <ul role="list" className="-my-6 divide-y divide-gray-200">
+          {getProducts().map((product) => (
+            <li key={product.id} className="flex py-6">
+              <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                <img
+                  alt={product.name}
+                  src={product.picture}
+                  className="size-full object-cover"
+                />
+              </div>
 
-      <div className="mx-auto max-w-2xl px-4 py-5 sm:px-6 sm:py- lg:max-w-7xl lg:px-8">
-        <div className="flow-root">
-          <ul role="list" className="-my-6 divide-y divide-gray-200">
-            {getProducts().map((product) => (
-              <li key={product.id} className="flex py-6">
-                <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                  <img
-                    alt={product.name}
-                    src={product.picture}
-                    className="size-full object-cover"
-                  />
-                </div>
+              <div className="ml-4 flex flex-1 flex-col">
+                <div>
+                  <div className="flex justify-between text-base font-medium">
+                    <Quantifier
+                      removeProductCallback={() =>
+                        handleRemoveProduct(product.id)
+                      }
+                      productId={product.id}
+                      handleUpdateQuantity={handleUpdateQuantity}
+                    />
 
-                <div className="ml-4 flex flex-1 flex-col">
-                  <div>
-                    <div className="flex justify-between text-base font-medium">
-                      <Quantifier
-                        removeProductCallback={() =>
-                          handleRemoveProduct(product.id)
-                        }
-                        productId={product.id}
-                        handleUpdateQuantity={handleUpdateQuantity}
-                      />
-
-                      <p className="ml-4">{product.price}</p>
-                    </div>
-                  </div>
-                  <div className="flex flex-1 items-end justify-between text-sm">
-                    <p>Qty {product.quantity}</p>
-
-                    <div className="flex">
-                      <button
-                        type="button"
-                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                      >
-                        Remove
-                      </button>
-                    </div>
+                    <p className="ml-4">{product.price}</p>
                   </div>
                 </div>
-              </li>
-            ))}
-          </ul>
+                <div className="flex flex-1 items-end justify-between text-sm">
+                  <p>Qty {product.quantity}</p>
+
+                  <div className="flex">
+                    <button
+                      type="button"
+                      className=" button font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+        <div className="flex justify-between text-base font-medium ">
+          <p>Total</p>
+          <p>{totalPrice} €</p>
+        </div>
+        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-xl text-white font-semibold py-2 px-4 rounded-xl"
+          >
+            Checkout
+          </button>
+        </div>
+        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+          <p>
+            or{" "}
+            <button
+              type="button"
+              onClick={navigateToShop}
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Continue Shopping
+              <span aria-hidden="true"> &rarr;</span>
+            </button>
+          </p>
         </div>
       </div>
-
-      <p>{totalPrice}</p>
-    </section>
+    </div>
   );
 };
