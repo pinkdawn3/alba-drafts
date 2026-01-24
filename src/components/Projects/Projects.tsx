c836db99480e3c8423c538803c74927917f24264
Alba <albadetaoro@gmail.com>
2026-01-24 00:51:58 +0000

    feat: created shop gallery

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router";
+import { Shop } from "./Shop/Shop";
+
 function Projects() {
   return (
     <main>
+      <nav>
+        <NavLink to="/projects/shop" className={"links-header"}>
+          Tienda
+        </NavLink>
+      </nav>
       <p>Aquí van los proyectos que hago.</p>
+
+      <Shop />
     </main>
   );
 }
