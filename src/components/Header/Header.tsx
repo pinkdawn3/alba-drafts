ba955a1de3deb3cabf0ce64541d217e196fb44a1
Alba <albadetaoro@gmail.com>
2026-01-24 16:40:42 +0000

    style: started styling with tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,10 @@ import { NavLink } from "react-router";
 
 function Header() {
   return (
-    <nav className="header">
-      <NavLink to="/" className={"links-header"}>
-        Home
-      </NavLink>
-      <NavLink to="/projects" className={"links-header"}>
-        Projects
-      </NavLink>
-      <NavLink to="/about" className={"links-header"}>
-        About me
-      </NavLink>
+    <nav className="py-5 px-5 shadow-xl space-x-5 font-bold">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/projects">Projects</NavLink>
+      <NavLink to="/about">About me</NavLink>
     </nav>
   );
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,17 +1,13 @@
 import { NavLink } from "react-router";
-import { Shop } from "./Shop/Shop";
+import { Products } from "./Shop/Products/Products";
 
 function Projects() {
   return (
     <main>
-      <nav>
-        <NavLink to="/projects/shop" className={"links-header"}>
-          Tienda
-        </NavLink>
+      <nav className="px-10 py-5 font-semibold">
+        <NavLink to="/projects/shop">Tienda</NavLink>
       </nav>
-      <p>Aquí van los proyectos que hago.</p>
-
-      <Shop />
+      <Products />
     </main>
   );
 }
