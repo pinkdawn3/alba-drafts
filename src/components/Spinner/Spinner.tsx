b79a85bf1bea274b710b057f6bff1f75a00c8188
Alba <albadetaoro@gmail.com>
2026-01-25 15:21:54 +0000

    feat: added spinner for fetchdata wait in product list

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,16 @@
+import type { FunctionComponent } from "react";
+
+export const Spinner: FunctionComponent = () => {
+  return (
+    <div className="flex py-10 justify-center">
+      <div
+        className="inline-block h-10 w-10 animate-[spin_1.5s_linear_infinite] rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_2s_linear_infinite]"
+        role="status"
+      >
+        <span className="absolute! -m-px! h-px! w-px! overflow-hidden! whitespace-nowrap! border-0! p-0! [clip:rect(0,0,0,0)]!">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
+};
