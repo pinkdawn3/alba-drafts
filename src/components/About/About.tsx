5de05e1510fdad6df08122f33ff757c5d48b0125
Alba <albadetaoro@gmail.com>
2026-01-25 19:51:36 +0000

    lan: translated shop

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,38 @@
 import ProfilePic from "../../assets/profile-pic.jpg";
 import CVEnglish from "../../assets/AlbaDeTaoro_SoftwareDeveloper_CV (English).pdf";
 import CVSpanish from "../../assets/AlbaDeTaoro_SoftwareDeveloper_CV (Español).pdf";
+import { Trans } from "@lingui/react/macro";
+import { i18n } from "@lingui/core";
 
 function About() {
+  const handleCVLanguage = (button: string) => {
+    switch (button) {
+      case "primary":
+        if (i18n.locale == "es") {
+          return CVSpanish;
+        } else if (i18n.locale == "en") {
+          return CVEnglish;
+        }
+
+        break;
+
+      case "secondary":
+        if (i18n.locale == "es") {
+          return CVEnglish;
+        } else if (i18n.locale == "en") {
+          return CVSpanish;
+        }
+
+        break;
+
+      default:
+        return CVEnglish;
+    }
+  };
+
+  const CVPrimary = handleCVLanguage("primary");
+  const CVSecondary = handleCVLanguage("secondary");
+
   return (
     <main className="sm:flex items-center max-w-7xl">
       <div className="sm:w-1/2 p-10 flex justify-content">
@@ -13,53 +43,59 @@ function About() {
       <div className="sm:w-1/2 p-5">
         <div className="text">
           <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">
-            About Me
+            <Trans>About Me</Trans>
           </span>
           <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
             Alba <span className="text-indigo-600">De Taoro González</span>
           </h2>
           <div className="text-gray-200 space-y-4">
             <p>
-              My professional journey didn't start in software development. I
-              originally studied Translation and interpreting, but I had the
-              misfortune of having to enter the job market in 2020.
+              <Trans>
+                My professional journey didn't start in software development. I
+                originally studied Translation and interpreting, but I had the
+                misfortune of having to enter the job market in 2020.
+              </Trans>
             </p>
             <p>
-              I felt stuck so I decided to reinvent myself. Apart from
-              languages, my other great passion since I was a child was
-              computers. So, I studied software development with my initial
-              intention being working on Game Development.
+              <Trans>
+                I felt stuck so I decided to reinvent myself. Apart from
+                languages, my other great passion since I was a child was
+                computers. So, I studied software development with my initial
+                intention being working on Game Development.
+              </Trans>
             </p>
             <p>
-              Along the way, however, I uncovered a passion for web development
-              and front-end, and here I am now. I am excited to keep learning
-              and creating new things along the way.
+              <Trans>
+                Along the way, however, I uncovered a passion for web
+                development and front-end, and here I am now. I am excited to
+                keep learning and creating new things along the way.
+              </Trans>
             </p>
 
             <div className="flex items-center">
               <a
                 aria-label="download cv in english"
                 className="button text-center"
-                href={CVEnglish}
+                href={CVPrimary}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Download CV
+                <Trans>Download CV</Trans>
               </a>
 
               <p className="pl-5 text-center">
                 <span className=" whitespace-pre-wrap">
-                  ...or download it in{" "}
+                  <Trans>...or download it in </Trans>
                 </span>
 
                 <a
                   aria-label="download cv in spanish"
                   className="text-blue-500 underline hover:cursor-pointer"
-                  href={CVSpanish}
+                  href={CVSecondary}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Spanish
+                  <Trans id="cv_language">Spanish</Trans>
                 </a>
               </p>
             </div>
--- src/components/Projects/Shop/Cart/Cart.tsx
+++ src/components/Projects/Shop/Cart/Cart.tsx
@@ -4,6 +4,7 @@ import useLocalStorageState from "use-local-storage-state";
 import type { CartProps } from "../Products/Products";
 import { type Operation, Quantifier } from "../Quantifier/Quantifier";
 import { useNavigate } from "react-router";
+import { Trans } from "@lingui/react/macro";
 
 export const Cart: FunctionComponent = () => {
   const [cart, setCart] = useLocalStorageState<CartProps>("cart", {});
@@ -49,51 +50,59 @@ export const Cart: FunctionComponent = () => {
     0,
   );
 
+  console.log(getProducts());
+
   return (
     <div className="mx-auto max-w-2xl px-4 py-5 sm:px-6 sm:py- lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl font-bold tracking-tight text-gray-200">Cart</h2>
       <div className="flow-root py-5">
         <ul role="list" className="-my-6 divide-y divide-gray-200">
-          {getProducts().map((product) => (
-            <li key={product.id} className="flex py-6">
-              <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                <img
-                  alt={product.name}
-                  src={product.picture}
-                  className="size-full object-cover"
-                />
-              </div>
+          {getProducts().length === 0 ? (
+            <p className="py-6 mx-auto">
+              <Trans>The cart is empty.</Trans>
+            </p>
+          ) : (
+            getProducts().map((product) => (
+              <li key={product.id} className="flex py-6">
+                <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                  <img
+                    alt={product.name}
+                    src={product.picture}
+                    className="size-full object-cover"
+                  />
+                </div>
 
-              <div className="ml-4 flex flex-1 flex-col">
-                <div>
-                  <div className="flex justify-between text-base font-medium">
-                    <Quantifier
-                      removeProductCallback={() =>
-                        handleRemoveProduct(product.id)
-                      }
-                      productId={product.id}
-                      handleUpdateQuantity={handleUpdateQuantity}
-                    />
+                <div className="ml-4 flex flex-1 flex-col">
+                  <div>
+                    <div className="flex justify-between text-base font-medium">
+                      <Quantifier
+                        removeProductCallback={() =>
+                          handleRemoveProduct(product.id)
+                        }
+                        productId={product.id}
+                        handleUpdateQuantity={handleUpdateQuantity}
+                      />
 
-                    <p className="ml-4">{product.price}</p>
+                      <p className="ml-4">{product.price}</p>
+                    </div>
+                  </div>
+                  <div className="flex flex-1 items-end justify-between text-sm">
+                    <p>Qty {product.quantity}</p>
+
+                    <div className="flex">
+                      <button
+                        type="button"
+                        className=" button font-medium text-indigo-600 hover:text-indigo-500"
+                        onClick={() => handleRemoveProduct(product.id)}
+                      >
+                        <Trans> Remove</Trans>
+                      </button>
+                    </div>
                   </div>
                 </div>
-                <div className="flex flex-1 items-end justify-between text-sm">
-                  <p>Qty {product.quantity}</p>
-
-                  <div className="flex">
-                    <button
-                      type="button"
-                      className=" button font-medium text-indigo-600 hover:text-indigo-500"
-                      onClick={() => handleRemoveProduct(product.id)}
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))
+          )}
         </ul>
       </div>
       <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
@@ -111,13 +120,15 @@ export const Cart: FunctionComponent = () => {
         </div>
         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
           <p>
-            or{" "}
+            <Trans>or </Trans>
+
             <button
               type="button"
               onClick={navigateToShop}
               className="font-medium text-indigo-600 hover:text-indigo-500"
             >
-              Continue Shopping
+              <Trans>Continue Shopping</Trans>
+
               <span aria-hidden="true"> &rarr;</span>
             </button>
           </p>
