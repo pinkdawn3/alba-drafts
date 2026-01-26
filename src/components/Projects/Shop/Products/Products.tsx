7b4cede45f25ed491bbfab497dba538a79ac17f3
Alba <albadetaoro@gmail.com>
2026-01-26 13:55:27 +0000

    finished with light theme

--- src/components/Projects/Shop/Products/Products.tsx
+++ src/components/Projects/Shop/Products/Products.tsx
@@ -65,7 +65,7 @@ export const Products: FunctionComponent = () => {
     <section>
       <div className="mx-auto max-w-2xl px-4 py-5 sm:px-6 sm:py- lg:max-w-7xl lg:px-8">
         <div className="flex justify-between">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-200">
+          <h2 className="text-2xl font-bold tracking-tight text-font">
             <Trans>Products</Trans>
           </h2>
 
