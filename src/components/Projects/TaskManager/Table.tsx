227e5825adf21f193f35444ec61cb0ce05581cf0
Alba <albadetaoro@gmail.com>
2026-01-26 20:48:15 +0000

    added dropdown to priority

--- src/components/Projects/TaskManager/Table.tsx
+++ src/components/Projects/TaskManager/Table.tsx
@@ -1,3 +1,4 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import type { FunctionComponent } from "react";
 
 type TaskType = {
@@ -30,7 +31,60 @@ const TaskMockup: TaskType[] = [
     priority: "low",
     completed: false,
   },
+  {
+    id: 4,
+    taskName: "Create columns dinamically",
+    assigned: "Alba",
+    priority: "low",
+    completed: false,
+  },
 ];
+
+const Dropdown: FunctionComponent = () => {
+  return (
+    <>
+      <Menu as="div" className="relative inline-block">
+        <MenuButton className={"link-style"}> Priority</MenuButton>
+
+        <MenuItems
+          transition
+          className="absolute z-10 top-0 mb-2 w-56 origin-top-right rounded-md bg-zinc-700 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+        >
+          <div className="py-1">
+            <MenuItem>
+              <button
+                type="button"
+                aria-label="Button for the Shop demo"
+                className="block w-full px-4 py-2 text-left text-sm bg-zinc-700"
+              >
+                High
+              </button>
+            </MenuItem>
+            <MenuItem>
+              <button
+                type="button"
+                aria-label="Button for the Task Manager demo"
+                className="block w-full px-4 py-2 text-left text-sm bg-zinc-700"
+              >
+                Medium
+              </button>
+            </MenuItem>
+
+            <MenuItem>
+              <button
+                type="button"
+                aria-label="Button for the Task Manager demo"
+                className="block w-full px-4 py-2 text-left text-sm bg-zinc-700"
+              >
+                Low
+              </button>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </Menu>
+    </>
+  );
+};
 
 export const Table: FunctionComponent = () => {
   const generateRow = (task: TaskType) => {
@@ -43,7 +97,7 @@ export const Table: FunctionComponent = () => {
           {task.assigned}
         </td>
         <td className="border-t-0 px-4 text-xs font-medium whitespace-nowrap p-4">
-          {task.priority}
+          <Dropdown />
         </td>
         <td className="border-t-0 px-4 text-xs font-medium whitespace-nowrap p-4">
           <input type="checkbox" aria-label="Checkbox for completed task" />
@@ -61,13 +115,13 @@ export const Table: FunctionComponent = () => {
             <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
               Task
             </th>
-            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
+            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold  uppercase border-l-0 border-r-0 whitespace-nowrap">
               Assigned to
             </th>
-            <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
+            <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
               Priority
             </th>
-            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
+            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
               Completed
             </th>
           </tr>
