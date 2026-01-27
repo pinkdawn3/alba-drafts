f9babe561b8201515d064dcf78b9ae01b900a3f5
Alba <albadetaoro@gmail.com>
2026-01-27 19:07:49 +0000

    feat: integrated task provider with task table

--- src/components/Projects/TaskManager/Table.tsx
+++ src/components/Projects/TaskManager/Table.tsx
@@ -1,54 +1,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import type { FunctionComponent } from "react";
-
-type TaskType = {
-  id: number;
-  taskName: string;
-  assigned: string;
-  priority: string;
-  status: string;
-  date: number;
-  completed: boolean;
-};
-
-const TaskMockup: TaskType[] = [
-  {
-    id: 1,
-    taskName: "Make Table",
-    assigned: "Alba",
-    priority: "medium",
-    status: "Not started",
-    date: Date.now(),
-    completed: false,
-  },
-  {
-    id: 2,
-    taskName: "Make rows dynamic",
-    assigned: "Alba",
-    priority: "high",
-    status: "Not started",
-    date: Date.now(),
-    completed: false,
-  },
-  {
-    id: 3,
-    taskName: "Save data in localstorage",
-    assigned: "Alba",
-    priority: "low",
-    status: "Not started",
-    date: Date.now(),
-    completed: false,
-  },
-  {
-    id: 4,
-    taskName: "Create columns dinamically",
-    assigned: "Alba",
-    priority: "low",
-    status: "Not started",
-    date: Date.now(),
-    completed: false,
-  },
-];
+import { useTasks } from "../../../hooks/useTasks";
+import type { TaskType } from "../../../types/task";
 
 const Dropdown: FunctionComponent = () => {
   return (
@@ -98,12 +51,29 @@ const Dropdown: FunctionComponent = () => {
 
 export const Table: FunctionComponent = () => {
   const esFormatter = new Intl.DateTimeFormat("es-ES");
+  const { getTasks, dispatch } = useTasks();
 
   const generateRow = (task: TaskType) => {
+    const taskId = task.id;
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      dispatch({
+        type: "update",
+        id: taskId,
+        changes: {
+          taskName: e.target.value,
+        },
+      });
+    };
     return (
       <tr key={task.id} className="text-white">
         <th className="border-t-0 px-4 text-sm font-normal whitespace-nowrap p-4 text-left">
-          {task.taskName}
+          <input
+            type="text"
+            placeholder="Introduce task..."
+            value={task.taskName}
+            onChange={handleChange}
+          />
         </th>
         <td className="border-t-0 px-4 text-xs font-medium whitespace-nowrap p-4">
           {task.assigned}
@@ -125,38 +95,55 @@ export const Table: FunctionComponent = () => {
   };
 
   return (
-    <div className="w-full overflow-x-auto border border-zinc-700 rounded-lg">
-      <table className="items-center w-full bg-transparent border-collapse rounded-lg text-center [&_:is(th,td):first-child]:text-left">
-        {/* Header for table */}
-        <thead>
-          <tr className="text-center">
-            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
-              Task
-            </th>
-            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold  uppercase border-l-0 border-r-0 whitespace-nowrap">
-              Assigned to
-            </th>
-            <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
-              Priority
-            </th>
-            <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
-              Status
-            </th>
-            <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
-              Date
-            </th>
-            <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
-              Completed
-            </th>
-          </tr>
-        </thead>
-        {/* Body of table*/}
-        <tbody className="divide-y divide-zinc-700">
-          {/* 1 Row*/}
+    <section className="w-full space-y-5">
+      <button type="button" aria-label="add task" className="button">
+        Add Task
+      </button>
 
-          {TaskMockup.map((task: TaskType) => generateRow(task))}
-        </tbody>
-      </table>
-    </div>
+      <div className="overflow-x-auto border border-zinc-700 rounded-lg">
+        <table className="items-center w-full bg-transparent border-collapse rounded-lg text-center [&_:is(th,td):first-child]:text-left">
+          {/* Header for table */}
+          <thead>
+            <tr className="text-center">
+              <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">
+                Task
+              </th>
+              <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold  uppercase border-l-0 border-r-0 whitespace-nowrap">
+                Assigned to
+              </th>
+              <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
+                Priority
+              </th>
+              <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
+                Status
+              </th>
+              <th className="px-4 bg-zinc-700 text-white align-middle py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap">
+                Date
+              </th>
+              <th className="px-4 bg-zinc-700 text-white py-3 text-xs font-semibold uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
+                Completed
+              </th>
+            </tr>
+          </thead>
+          {/* Body of table*/}
+          <tbody className="divide-y divide-zinc-700">
+            {/* 1 Row*/}
+
+            {getTasks().length === 0 ? (
+              <tr className="text-white">
+                <th
+                  key={1}
+                  className="border-t-0 px-4 text-sm font-normal whitespace-nowrap p-4 text-left"
+                >
+                  No tasks available
+                </th>
+              </tr>
+            ) : (
+              getTasks().map((task) => generateRow(task))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 };
--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -1,10 +1,11 @@
 import { createContext } from "react";
-import type { TaskProps } from "../types/task";
+import type { TaskProps, TaskType } from "../types/task";
 import type { Action } from "./TasksProvider";
 
 export type TaskContextType = {
   tasks: TaskProps;
   dispatch: React.Dispatch<Action>;
+  getTasks: () => TaskType[];
 };
 
 export const TasksContext = createContext<TaskContextType | undefined>(
