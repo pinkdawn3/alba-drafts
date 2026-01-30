eaefd3ad26f1be535901ce6a8e9aff6babda7b95
Alba <albadetaoro@gmail.com>
2026-01-30 21:40:42 +0000

    Updates

--- assets/CartWidget-C7nBP-Yv.js
+++
@@ -1,2 +0,0 @@
-import{a as n,j as a,F as o,h as c}from"./index-RNaslhL_.js";function r({productsCount:s}){const t=n(),e=async()=>{await t("/projects/shop/cart")};return a.jsxs("button",{onClick:e,className:"flex items-center space-x-2",children:[a.jsx("span",{className:"text-lg",children:s}),a.jsx("span",{children:a.jsx(o,{icon:c})})]})}export{r as default};
-//# sourceMappingURL=CartWidget-C7nBP-Yv.js.map
