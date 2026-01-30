9e25a5161e1814d793e7017cc0c530284d4e4bac
Alba <albadetaoro@gmail.com>
2026-01-30 21:20:19 +0000

    Updates

---
+++ assets/CartWidget-Cnw_EgDj.js
@@ -0,0 +1,2 @@
+import{a as n,j as a,F as o,h as c}from"./index-D_46N8Iw.js";function r({productsCount:s}){const t=n(),e=async()=>{await t("/projects/shop/cart")};return a.jsxs("button",{onClick:e,className:"flex items-center space-x-2",children:[a.jsx("span",{className:"text-lg",children:s}),a.jsx("span",{children:a.jsx(o,{icon:c})})]})}export{r as default};
+//# sourceMappingURL=CartWidget-Cnw_EgDj.js.map
