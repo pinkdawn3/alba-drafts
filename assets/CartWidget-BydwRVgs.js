ee11f45d3caa7c9996580e3d52883217e7c8e09b
Alba <albadetaoro@gmail.com>
2026-02-06 17:44:22 +0000

    Updates

--- assets/CartWidget-BydwRVgs.js
+++
@@ -1,2 +0,0 @@
-import{a as n,j as a,F as o,e as c}from"./index-D4CYMrGM.js";function r({productsCount:s}){const t=n(),e=async()=>{await t("/projects/shop/cart")};return a.jsxs("button",{onClick:e,className:"flex items-center space-x-2",children:[a.jsx("span",{className:"text-lg",children:s}),a.jsx("span",{children:a.jsx(o,{icon:c})})]})}export{r as default};
-//# sourceMappingURL=CartWidget-BydwRVgs.js.map
