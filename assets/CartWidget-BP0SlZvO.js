5f58ddc63a21fb7ae87b0b6886c2d04269e6085a
Alba <albadetaoro@gmail.com>
2026-02-15 16:58:42 +0000

    Updates

---
+++ assets/CartWidget-BP0SlZvO.js
@@ -0,0 +1,2 @@
+import{a as n,j as a,F as o,e as c}from"./index-q2G-m5l6.js";function r({productsCount:s}){const t=n(),e=async()=>{await t("/projects/shop/cart")};return a.jsxs("button",{onClick:e,className:"flex items-center space-x-2",children:[a.jsx("span",{className:"text-lg",children:s}),a.jsx("span",{children:a.jsx(o,{icon:c})})]})}export{r as default};
+//# sourceMappingURL=CartWidget-BP0SlZvO.js.map
