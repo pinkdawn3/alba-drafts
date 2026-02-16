0b81c771c33ab7817ddcb997fa0e442e100c2682
Alba <albadetaoro@gmail.com>
2026-02-16 19:16:12 +0000

    Updates

--- assets/CartWidget-DLPt4Jah.js
+++
@@ -1,2 +0,0 @@
-import{a as n,j as a,F as o,e as c}from"./index-C2mu4T3D.js";function r({productsCount:s}){const t=n(),e=async()=>{await t("/projects/shop/cart")};return a.jsxs("button",{onClick:e,className:"flex items-center space-x-2",children:[a.jsx("span",{className:"text-lg",children:s}),a.jsx("span",{children:a.jsx(o,{icon:c})})]})}export{r as default};
-//# sourceMappingURL=CartWidget-DLPt4Jah.js.map
